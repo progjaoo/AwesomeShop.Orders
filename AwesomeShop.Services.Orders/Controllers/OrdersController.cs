2d6599dd94f677f8b891e06bc0c59f2319510911
Joao marcos valente <joaomarcoslars.81@gmail.com>
2023-07-25 12:53:36 -0300

    Feat: Implementando o OrdersController na camada API com métodos Get e Post

---
+++ AwesomeShop.Services.Orders/Controllers/OrdersController.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AwesomeShop.Orders.Application.Commands;
+using AwesomeShop.Orders.Application.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AwesomeShop.Services.Orders.API.Controllers
+{
+    [Route("api/customers/{customerId}/orders")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public OrdersController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Get(Guid id)
+        {
+            var query = new GetOrderById(id);
+
+            var result = await _mediator.Send(query);
+
+            if (result == null)
+                return NotFound();
+
+            return Ok(result);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] AddOrder command)
+        {
+            var id = await _mediator.Send(command);
+
+            return CreatedAtAction(nameof(Get), new { id = id }, command);
+        }
+    }
+}
